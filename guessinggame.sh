15c6185370fd29352f431f1b746b6b08c9629cff
dennysvande <vandesimbolon@gmail.com>
2020-07-08 20:10:44 +0700

    Commit all files

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,17 +5,21 @@ echo "How many files are there in this directory ?"
 echo "Enter your guess: "
 read guessVal
 
-while [[ $guessVal -ne $numFiles ]]
-do
-  if [[ $guessVal -gt $numFiles ]]
-  then
-    echo "Your guess is too high"
-  elif [[ $guessVal -lt $numFiles ]]
-  then
-   echo "Your guess is too low"
-  fi
-  echo "Enter your guess: "
-  read guessVal
-done
+function guessing_game {
+  while [[ $guessVal -ne $numFiles ]]
+  do
+    if [[ $guessVal -gt $numFiles ]]
+    then
+      echo "Your guess is too high"
+    elif [[ $guessVal -lt $numFiles ]]
+    then
+      echo "Your guess is too low"
+    fi
+    echo "Enter your guess: "
+    read guessVal
+  done
+}
+
+guessing_game
 
 echo "Congratulation your guess is correct!"
---
+++ makefile
@@ -0,0 +1,4 @@
+README.md:
+	echo "guessinggame.sh" >> README.md
+	echo $(date) >> README.md
+	echo $(ls -1 | wc -l) >> README.md    
